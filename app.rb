147bca2658a7a53f49a89256e3eae43191159c62
Alexander Jacks <alexd.jacks@gmail.com>
2016-07-15 14:54:38 -0700

    adds client and stylist pages with test db entries

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
+require 'PG'
 require 'sinatra'
 require 'sinatra/reloader'
 also_reload 'lib/**/*.rb'
 require './lib/stylist'
 require './lib/client'
+require 'pry'
+DB = PG.connect({:dbname => 'hair_salon_test'})
+
+get '/' do
+  erb :index
+end
+get '/stylist' do
+  @stylists = Stylist.all()
+  erb :stylist
+end
+get '/client' do
+  @clients = Client.all()
+  erb :client
+end
