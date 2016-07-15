ef3ebf78dac03567b80817eb38dc9dc898d58877
Alexander Jacks <alexd.jacks@gmail.com>
2016-07-15 15:27:20 -0700

    provides ability to Add stylists and clients

--- app.rb
+++ app.rb
@@ -14,7 +14,30 @@ get '/stylist' do
   @stylists = Stylist.all()
   erb :stylist
 end
+get '/new_stylist_form' do
+  erb :new_stylist_form
+end
+post '/new_stylist_form' do
+  name = params.fetch('new_stylist_name')
+  contact = params.fetch('new_stylist_contact')
+  stylist = Stylist.new({:name => name, :contact => contact})
+  stylist.save()
+  @stylists = Stylist.all()
+  erb :stylist
+end
 get '/client' do
+  @clients = Client.all()
+  erb :client
+end
+get '/new_client_form' do
+  erb :new_client_form
+end
+post '/new_client_form' do
+  name = params.fetch('new_client_name')
+  contact = params.fetch('new_client_contact')
+  stylist_id = params.fetch('new_client_stylist_id')
+  client = Client.new({:name => name, :contact => contact, :stylist_id => stylist_id})
+  client.save()
   @clients = Client.all()
   erb :client
 end
